44d35731b77bc4076013a33df4f39fce9186e238
SwiftKobe <folledoswag@gmail.com>
2018-02-20 16:43:16 -0500

    Initial Commit

---
+++ FolledoChecklist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FolledoChecklist
+//
+//  Created by Samuel Folledo on 2/20/18.
+//  Copyright © 2018 Samuel Folledo. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
